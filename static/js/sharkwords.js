85df0f984ef097876149e6c409d5437d95bb1fbf
Lucy Gavrich <annalgav@gmail.com>
2022-08-30 17:31:34 -0700

    Finished everything for afternoon lab except play again

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 
 const WORDS = [
@@ -15,7 +17,7 @@ const WORDS = [
   'chocolate',
 ];
 
-const numWrong = 0;
+let numWrong = 0;
 
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
@@ -29,7 +31,7 @@ const createDivsForChars = (word) => {
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
   for (const letter of ALPHABET) {
-    document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend', `<button>${letter}</button>`);
+    document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend', `<button name=${letter} id=${letter}>${letter}</button>`);
   }
 };
 
@@ -46,8 +48,40 @@ const disableLetterButton = (buttonEl) => {
 // For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  if (document.querySelector(`.${letter}`)) {
+    return true;
+  }
 };
+
+const handleCorrectGuess = (letter) => {
+  const spaces = document.querySelectorAll(`.${letter}`);
+  
+  for (const space of spaces) {
+    space.insertAdjacentHTML('beforeend', `${letter}`);
+  }
+
+  const rightButton = document.querySelector(`#${letter}`);
+  rightButton.setAttribute('disabled', true);
+
+  
+};
+
+const handleWrongGuess = (letter) => {
+  if (numWrong === 5) {
+    for (const button of document.querySelectorAll('button')) {
+      button.setAttribute('disabled', true);
+    }
+
+    document.querySelector('a').style.display = '';
+  } else {
+    numWrong += 1;
+    const sharkPhoto = document.querySelector('img');
+    sharkPhoto.setAttribute('src', `/static/images/guess${numWrong}.png`);
+    const wrongButton = document.querySelector(`#${letter}`);
+    wrongButton.setAttribute('disabled', true);
+  }
+}
+
 
 // This is like if __name__ == '__main__' in Python
 // It will be called when the file is run (because
@@ -57,6 +91,7 @@ const isLetterInWord = (letter) => {
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
+  // const word = WORDS[Math.floor(Math.random()*WORDS.length)];
   const word = 'hello';
 
   // call the function that makes an empty line for each letter in the word
@@ -64,6 +99,21 @@ const isLetterInWord = (letter) => {
 
   // call the function that makes a button for each letter in the alphabet
   generateLetterButtons();
+
+  const buttons = document.querySelectorAll('button');
+
+  for (const button of buttons) {
+    button.addEventListener('click', () => {
+    const letter = button.name;
+    if (isLetterInWord(letter)) {
+      handleCorrectGuess(letter);
+    } else {
+      handleWrongGuess(letter);
+    }
+    });
+  }
+
+  
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
